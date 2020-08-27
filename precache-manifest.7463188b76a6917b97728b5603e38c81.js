4a249f3ee7a931132ac3e4f07f4bd667f1fc0883
RNaoki <Ricardo.N.Tanji@gmail.com>
2020-08-27 13:54:43 -0300

    Updates

---
+++ precache-manifest.7463188b76a6917b97728b5603e38c81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23e39bb9d9e4daec926fd38b25037988",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "799ac48c18937ecf27db",
+    "url": "/todo/static/css/main.c8f059a3.chunk.css"
+  },
+  {
+    "revision": "d05826bec8223c792b76",
+    "url": "/todo/static/js/2.d76ec85c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todo/static/js/2.d76ec85c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "799ac48c18937ecf27db",
+    "url": "/todo/static/js/main.44c9e833.chunk.js"
+  },
+  {
+    "revision": "aca1e5bce2a95c62b0a6",
+    "url": "/todo/static/js/runtime-main.0fecab1a.js"
+  }
+]);
